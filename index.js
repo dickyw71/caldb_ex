9ef769a61d2dc81a09cfaf915b72928208019f4b
Richard Wheatley <dickyw71@users.noreply.github.com>
2019-08-06 20:52:43 +0100

    Minor mods.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var oracledb = require('oracledb');
 
+console.time('db-connect-select-release');
+console.time('db-connect');
 // Get a non-pooled connection
 oracledb.getConnection(
     {
@@ -12,6 +14,8 @@ oracledb.getConnection(
         console.error(err.message);
         return;
       }
+      console.timeEnd('db-connect');
+      console.time('db-select');
       connection.execute(
         // The statement to execute
         `SELECT bar_code as barcode
@@ -20,16 +24,19 @@ oracledb.getConnection(
         FROM sensor s 
         JOIN sensor_calibration sc 
             ON(s.sensor_id = sc.sensor_id)
-        WHERE bar_code LIKE 'TZ000213'`,
+        WHERE bar_code = :barcode`,
   
         // The "bind value" is null
-        {},
+        ['TZ000014'],
   
         // execute() options argument.  Since the query only returns one
         // row, we can optimize memory usage by reducing the default
         // maxRows value.  For the complete list of other options see
         // the documentation.
         { maxRows: 1
+          , fetchInfo : {
+            "CAL_DUE_DATE": { type : oracledb.STRING }
+          }
           //, outFormat: oracledb.OBJECT  // query result format
           //, extendedMetaData: true      // get extra metadata
           //, fetchArraySize: 100         // internal buffer allocation size for tuning
@@ -42,8 +49,10 @@ oracledb.getConnection(
             doRelease(connection);
             return;
           }
+          console.timeEnd('db-select');
           console.log(result.metaData); // [ { name: 'DEPARTMENT_ID' }, { name: 'DEPARTMENT_NAME' } ]
           console.log(result.rows);     // [ [ 180, 'Construction' ] ]
+          console.time('db-release');
           doRelease(connection);
         });
     });
@@ -55,5 +64,7 @@ oracledb.getConnection(
         if (err) {
           console.error(err.message);
         }
+        console.timeEnd('db-release');
+        console.timeEnd('db-connect-select-release');
       });
   }
